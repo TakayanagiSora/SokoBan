8fc4c72a17fafd9c2cc0633257073d8d6f405bd4
STD\s20227082 <20227082-takayanagisora@hcs.ac.jp>
2024-01-24 16:38:47 +0900

    追加 >> ちょっと

--- Assets/Scripts/Goal/Goal.cs
+++ Assets/Scripts/Goal/Goal.cs
@@ -2,17 +2,8 @@ using UnityEngine;
 
 public class Goal : MonoBehaviour
 {
-    private MapController _mapController = default;
-    private bool _isGoal = false;
-
-
-    private void Awake()
-    {
-        _mapController = FindObjectOfType<MapController>();
-    }
-
     public void OnGoal()
     {
-        _isGoal = true;
+        
     }
 }
